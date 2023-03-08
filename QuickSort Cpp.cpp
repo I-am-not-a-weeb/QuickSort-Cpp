e2811b6416622b2de4667b55ee3ba03776b341dc
I_am_not_a_weeb <kubutek04@gmail.com>
2023-03-08 21:06:44 +0100

    Dodaj pliki projektów.

---
+++ QuickSort Cpp.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+void quickSort(int*arr,int p, int r)
+{
+
+}
+void partition(int*arr,int p,int r)
+{
+	int x = arr[r];
+	int i = p - 1;
+	for (int j = p; j <= r;j++)
+	{
+		if (arr[j] <= x)
+		{
+			i++;
+
+		}
+	}
+}
+
+
+int main()
+{
+   
+}
